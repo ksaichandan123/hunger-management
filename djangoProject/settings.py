0773360eb8d540282ad41caa577077054fa03d70
ksaichandan123 <ksaichandan123@gmail.com>
2023-12-07 19:09:48 +0000

    lotsof changes

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
 
 # SESSION_EXPIRE_SECONDS = 3600  # 1 hour
